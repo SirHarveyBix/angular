1db93cdf9a614f19bf8083bd38caafe6c2918fc4
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-07-10 18:47:53 +0200

    data binding

--- 00.getting-started/src/app/servers/servers.component.ts
+++ 00.getting-started/src/app/servers/servers.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '[app-servers]',
-  template: `<app-server></app-server> <app-server></app-server>`,
+  // template: `<app-server></app-server> <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServers = false;
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServers = true;
+    }, 2000);
+  }
+}
