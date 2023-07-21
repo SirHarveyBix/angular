a061cbe0fe893a707bae1b401bb34e1a5461f676
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-07-21 17:03:56 +0200

    add :cockpit + server-element components and refacto from app

--- 02.components_databinding/src/app/app.component.ts
+++ 02.components_databinding/src/app/app.component.ts
@@ -3,26 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
+  serverElements = [
+    {
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+      name: 'Testserver',
+      content: 'just a test',
+    },
+  ];
+  constructor() {}
 }
