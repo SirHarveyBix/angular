80937d25edb5d38148596784a0a7fe443a3fed56
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-07-21 18:20:24 +0200

    play with encapsulation for style

--- 02.components_databinding/src/app/server-element/server-element.component.ts
+++ 02.components_databinding/src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
+  // change encapsulation policy for the component's style like : _ngcontent-ng-c962867798
+  encapsulation: ViewEncapsulation.None,
 })
 export class ServerElementComponent {
   @Input('srvElement') element: {
