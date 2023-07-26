25117433ec7d7e55001a50d16df122590c2bab0f
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-07-26 16:45:40 +0200

    bind data from recipes to list item and details

--- 03.recipe-components_databinding/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 03.recipe-components_databinding/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,11 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       'a Test Recipe',
       'this is a test',
       'https://static01.nyt.com/images/2022/09/29/dining/afg-ricotta-polpette/merlin_213671208_2553d655-4170-4155-acdf-5fca2ce34a06-articleLarge.jpg'
     ),
+    new Recipe(
+      'another Test Recipe',
+      'this is a new test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
