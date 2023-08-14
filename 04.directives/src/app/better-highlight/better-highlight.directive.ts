e43d5188527305925a85771ea2cbc80c11685a5a
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-14 13:26:41 +0200

    using @HostBinding to get rid of setStyle()

--- 04.directives/src/app/better-highlight/better-highlight.directive.ts
+++ 04.directives/src/app/better-highlight/better-highlight.directive.ts
@@ -1,6 +1,7 @@
 import {
   Directive,
   ElementRef,
+  HostBinding,
   HostListener,
   OnInit,
   Renderer2,
@@ -12,20 +13,19 @@ import {
 export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+
   @HostListener('mouseenter') mouseover(eventData: Event): void {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event): void {
-    this.renderer.setStyle(
-      this.elementRef.nativeElement,
-      'background-color',
-      'transparent'
-    );
+    this.backgroundColor = 'transparent';
   }
 
   ngOnInit(): void {
