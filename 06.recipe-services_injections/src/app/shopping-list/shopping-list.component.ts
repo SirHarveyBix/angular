4d6e327b97b3801242a2b727c75f70ba672f7286
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-16 12:47:58 +0200

    adding ShoppingListService app wide

--- 06.recipe-services_injections/src/app/shopping-list/shopping-list.component.ts
+++ 06.recipe-services_injections/src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.module';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('apple', 5),
-    new Ingredient('tomatoes', 7),
-  ];
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 }
