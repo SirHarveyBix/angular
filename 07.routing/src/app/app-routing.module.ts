4e410b98881bac768d92f910e3d0b0cfa8e2c2be
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-17 14:47:18 +0200

    .

--- 07.routing/src/app/app-routing.module.ts
+++ 07.routing/src/app/app-routing.module.ts
@@ -47,7 +47,12 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes
+      // { useHash: true }
+    ),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
