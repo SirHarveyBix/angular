08667057e6baaa7025544d9c4725c3cd6006ba53
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-17 14:49:18 +0200

    int recipe book project for implementing routes

---
+++ 08.recipe-routing/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,23 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.module';
+import { ShoppingListService } from '../shopping-list.service';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css'],
+})
+export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: true }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInputRef: ElementRef;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameInputRef.nativeElement.value,
+      this.amountInputRef.nativeElement.value
+    );
+    this.shoppingListService.addIngredient(newIngredient);
+  }
+}
