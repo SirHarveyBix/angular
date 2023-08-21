cc0f4a72837e6e9b61fac2af122cebf10f961eff
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-21 14:08:59 +0200

    implement first and simple observable + destroy it

--- 09-observable/src/app/home/home.component.ts
+++ 09-observable/src/app/home/home.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObserverSubscription: Subscription;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // custom observable
+    this.firstObserverSubscription = interval(1000).subscribe((count) => {
+      console.log('%chome.component.ts count :', 'color: #007acc;', count);
+    });
+  }
+
+  // observer must be destory after use
+  ngOnDestroy(): void {
+    this.firstObserverSubscription.unsubscribe();
+  }
 }
