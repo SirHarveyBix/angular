862e8412ac5062ad9fdcbbe1e822122862a9f960
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-23 11:03:13 +0200

    init forms for main project

---
+++ 11.recipe-forms/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,24 @@
+import { Ingredient } from '../shared/ingredient.module';
+import { Subject } from 'rxjs';
+
+export class ShoppingListService {
+  ingredientsChanged = new Subject<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('apple', 5),
+    new Ingredient('tomatoes', 7),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+}
