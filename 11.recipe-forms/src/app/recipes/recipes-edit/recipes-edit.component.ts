b423dfed14f4e017a30573a0ea9d1979032dd107
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-23 18:33:44 +0200

    add : delete recipe, cancel button on edit, img path and cancel action

--- 11.recipe-forms/src/app/recipes/recipes-edit/recipes-edit.component.ts
+++ 11.recipe-forms/src/app/recipes/recipes-edit/recipes-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
 @Component({
@@ -15,6 +15,7 @@ export class RecipesEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private recipeService: RecipeService
   ) {}
 
@@ -32,6 +33,7 @@ export class RecipesEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onAddIngredient() {
@@ -41,6 +43,10 @@ export class RecipesEditComponent implements OnInit {
         amount: new FormControl(),
       })
     );
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   private initForm() {
