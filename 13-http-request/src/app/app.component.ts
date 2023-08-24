e8a2abc9cee3718658c798f908ea2ef39165f430
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-24 12:15:42 +0200

    init post en get http request, observable with subscribe

--- 13-http-request/src/app/app.component.ts
+++ 13-http-request/src/app/app.component.ts
@@ -11,18 +11,42 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPost();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    this.http
+      .post(
+        'https://angular-http-request-b3287-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(
+          '%capp.component.ts responseData',
+          'color: #007acc;',
+          responseData
+        );
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPost();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPost() {
+    this.http
+      .get(
+        'https://angular-http-request-b3287-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
+      )
+      .subscribe((posts) => {
+        console.log('%capp.component.ts posts', 'color: #007acc;', posts);
+      });
   }
 }
