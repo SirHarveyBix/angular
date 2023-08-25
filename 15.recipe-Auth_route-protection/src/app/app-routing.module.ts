ebe0530836637fa59a6c93eb051e13aaa95876ac
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-25 10:36:39 +0200

    init recipe app for authentication

---
+++ 15.recipe-Auth_route-protection/src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
+import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
+import { recipeResolverService } from './recipes/recipes-resolver.service';
+import { AuthComponent } from './auth/auth.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      { path: '', component: RecipesStartComponent },
+      { path: 'new', component: RecipesEditComponent },
+      {
+        path: ':id',
+        component: RecipeDetailComponent,
+        resolve: [recipeResolverService],
+      },
+      {
+        path: ':id/edit',
+        component: RecipesEditComponent,
+        resolve: [recipeResolverService],
+      },
+    ],
+  },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'auth', component: AuthComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
