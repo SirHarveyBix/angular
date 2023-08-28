6c2450296899a671f406221f724eaf7607720c84
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-28 15:26:16 +0200

    implements first standalone component !

--- 18.standalone-components/src/app/welcome/welcome.component.ts
+++ 18.standalone-components/src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // imports: [DetailsComponent], // If WelcomeComponent is standalone too !
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
 })
