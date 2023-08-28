62f9574f29ff6df128a6daa2efac8678c769a452
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-28 15:40:30 +0200

    handle standalone service

--- 18.standalone-components/src/main.ts
+++ 18.standalone-components/src/main.ts
@@ -3,9 +3,13 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(
+  AppComponent
+  // ,{  providers: [AnalyticsService], /* allows to share the state / instance app wide */ }
+);
