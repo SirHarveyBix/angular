12b0618bfd4491d0e307ecec62f15cb9039dcab6
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-28 15:54:29 +0200

    fix from previous lecture

--- 18.standalone-components/src/app/welcome/details/details.component.ts
+++ 18.standalone-components/src/app/welcome/details/details.component.ts
@@ -8,7 +8,7 @@ import { HighlightDirective } from 'src/app/shared/highlight.directive';
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
-  providers: [AnalyticsService], // adding a serviice this way, has its own state / instance
+  // providers: [AnalyticsService], adding a serviice this way, has its own state / instance
 })
 export class DetailsComponent {
   constructor(private analyticsService: AnalyticsService) {}
