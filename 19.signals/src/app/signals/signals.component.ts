c73ebf42954a477cb5b3a9a2646d68acbbc2fe1c
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-29 11:44:18 +0200

    update, output signals and updating with dirrerents methods

--- 19.signals/src/app/signals/signals.component.ts
+++ 19.signals/src/app/signals/signals.component.ts
@@ -8,16 +8,19 @@ import { Component, signal } from '@angular/core';
   imports: [NgFor],
 })
 export class SignalsComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
 
   increment() {
-    this.counter++;
-    this.actions.push('INCREMENT');
+    // this.counter.update((previousCounter) => previousCounter + 1);
+    this.counter.set(this.counter() + 1);
+
+    this.actions.mutate((previousActions) => previousActions.push('INCREMENT'));
   }
 
   decrement() {
-    this.counter--;
-    this.actions.push('DECREMENT');
+    // this.counter.update((previousCounter) => previousCounter - 1);
+    this.counter.set(this.counter() - 1);
+    this.actions.update((previousActions) => [...previousActions, 'DECREMENT']); // not previousActions.push(), only with mutate
   }
 }
