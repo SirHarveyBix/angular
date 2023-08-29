539357d5d37313b241299add2e45c528645b94d3
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-29 11:51:50 +0200

    playing with computed & effect

--- 19.signals/src/app/signals/signals.component.ts
+++ 19.signals/src/app/signals/signals.component.ts
@@ -1,5 +1,5 @@
 import { NgFor } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -10,11 +10,15 @@ import { Component, signal } from '@angular/core';
 export class SignalsComponent {
   actions = signal<string[]>([]);
   counter = signal(0);
+  doubleCounter = computed(() => this.counter() * 2); // computing value that depend on signals
+
+  constructor() {
+    effect(() => console.log(this.counter())); // rexecute this code every time this.counter changes, works as useEffect in React
+  }
 
   increment() {
     // this.counter.update((previousCounter) => previousCounter + 1);
     this.counter.set(this.counter() + 1);
-
     this.actions.mutate((previousActions) => previousActions.push('INCREMENT'));
   }
 
