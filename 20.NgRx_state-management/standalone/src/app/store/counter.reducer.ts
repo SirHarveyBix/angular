e3727c3f5cb7ef32e66e1d9f19f911f5b863c346
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-08-29 13:26:26 +0200

    get rid of services, add action to increment / decrement

--- 20.NgRx_state-management/standalone/src/app/store/counter.reducer.ts
+++ 20.NgRx_state-management/standalone/src/app/store/counter.reducer.ts
@@ -1,8 +1,13 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { decrement, increment } from './counter.action';
 
 const initialState = 0;
 
-export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1),
+  on(decrement, (state) => state - 1)
+);
 
 // old version it is working too !
 export function counterOldReducer(state = initialState) {
